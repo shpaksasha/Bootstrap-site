31af9a9189a4afa8d57e5dc6511c0b2e87d30a8a
shpaksasha <shpaksasha0000@gmail.com>
2020-12-06 21:50:28 +0200

    add many changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import Head from './components/Header/header';
+import Home from './components/Home/home';
+import Content from './components/Content/content';
+import Question from './components/FAQ/questions';
+import About from './components/About/about';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Head/>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/content' component={Content}/>
+            <Route exact path='/faq' component={Question}/>
+            <Route exact path='/about' component={About}/>
+        </Router>
+    )
+};
 
 export default App;
---
+++ src/components/Content/content.js
@@ -0,0 +1,141 @@
+import React from 'react';
+
+const Content = () =>{
+    return(
+        <div>
+           <h3>Sia - California Dreamin.mp3</h3>
+            <p>Основной контент</p>
+        </div>
+    )
+};
+
+export default Content;
+
+console.log('Post server');
+
+const up = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('preparing data');
+        const status = {
+            text: 'Hello world',
+            tel: 684245568,
+            status: 'online'
+        };
+        resolve(status)
+    }, 2000)
+});
+
+up.then(data =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            data.component = 'hell to heaven';
+            resolve(data)
+        },2000)
+    });
+}).then((clientData) =>{
+    console.log('Finish data', clientData)
+});
+
+
+const dilay = (ms)=>{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve()
+        }, ms)
+    })
+};
+
+// dilay(2000).then(()=> console.log('2 seconds'));
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+// function fetch() {
+//     console.log('Start');
+//      return dilay(2000)
+//          .then(() => fetch(url))
+//          .then(response => response.json())
+// }
+//
+// fetch()
+//     .then(data =>{
+//        console.log('Data: ', data)
+//     });
+
+
+
+async function fetchAsinc() {
+    console.log('Start');
+ await dilay(2000);
+  const response = await fetch(url);
+    const data = await response.json();
+    console.log('Data: ', data)
+}
+
+fetchAsinc();
+
+// let a =7;
+//
+// console.log(a);
+//
+// let b = new Promise((resolve, reject) =>{
+//     setTimeout(()=>{
+//        resolve(a = 99);
+//     }, 2000);
+// });
+//
+// b.then((prom)=>{
+//     console.log(prom)
+// });
+
+
+
+// const people = [
+//     {name: 'Sasha', age:26, cache: 1000},
+//     {name: 'Sergei', age:29, cache: 1200},
+//     {name: 'Lora', age:40, cache: 4000},
+//     {name: 'Vado', age:45, cache: 8000},
+//     {name: 'Julya', age:22, cache: 9000},
+// ];
+
+// for (let i=0; i<people.length; i++){
+//     console.log(people[i])
+// }
+
+// for (let person of people){
+//     console.log(person)
+// }
+
+//forEach
+// people.forEach(preson => console.log(preson));
+
+
+//Map
+
+// const newMas = people.map((person)=>{
+//     return `${person.name}: ${person.age}`
+// });
+// console.log(newMas);
+
+//Filter
+
+// const result =[];
+// for (let i=0; i<people.length; i++){
+//     if (people[i].age>=30){
+//         result.push(people[i])
+//     }
+// }
+// console.log(result)
+
+// const result = people.filter(person => person.age>=27);
+//     console.log(result);
+
+//Reduce
+// const mount = people.reduce((total, person) =>{
+//    return total + person.cache
+// },0);
+// console.log(mount);
+
+//Find
+
+// const vado = people.find(person => person.name ==='Vado');
+// console.log(vado);
